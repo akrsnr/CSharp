7a4ebf7f1e71d20aec59a95a0ede3918659a5237
akrsnr <sonerakarrr@gmail.com>
2016-08-20 17:38:34 +0300

    Update fizzbuzz.cs

--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -1,3 +1,11 @@
+/*
+
+The challenge is to print out all of the numbers from 1 to 100. Except if a number is a multiple of
+3, print out the word “Fizz” instead. If the number is a multiple of 5, print out “Buzz”. If a number
+is a multiple of both 3 and 5 (like 15 or 30) then print out “FizzBuzz”.
+
+*/
+
 using System;
 
 namespace ConsoleApplication3
