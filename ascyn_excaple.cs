594c8f80b55287ec51415ead30f1c85e9981ad4c
Soner <17342831+akrsnr@users.noreply.github.com>
2021-12-31 20:46:05 +0300

    Create ascyn_excaple.cs

---
+++ ascyn_excaple.cs
@@ -0,0 +1,42 @@
+class x
+{
+    static async Task Main(string[] args)
+    {
+        var q = Method1();
+        var y = Method2();
+        await Task.Delay(5000);
+        Console.WriteLine("soner");
+        await q;
+        await y;
+    }
+    public static async Task Method1()
+    {
+        Console.WriteLine("Method1 before");
+
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Method 1 : " + i);
+            }
+        });
+        
+        Console.WriteLine("Method1 after");
+
+    }
+    public static async Task Method2()
+    {
+        Console.WriteLine("Method2 before");
+
+        await Task.Run(() =>
+        {
+            for (int i = 101; i < 1110; i++)
+            {
+                Console.WriteLine("Method 2 : " + i);
+            }
+        });
+        
+        Console.WriteLine("Method2 after");
+
+    }
+}
